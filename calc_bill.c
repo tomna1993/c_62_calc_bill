ab66f84d0c868b67925beb8c11a2a6ff3646b630
TomasNagy <tomi.nagy@outlook.com>
2023-06-10 17:16:23 +0200

    feat: create menu structure

--- calc_bill.c
+++ calc_bill.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+#define CHARS_MAX 30
+#define ITEMS_MAX 30
+
+struct Menu
+{
+    char    Item[CHARS_MAX];
+    float   Price;
+};
+
 int main(void)
 {
-    
+    struct Menu snackBar[ITEMS_MAX];
+      
 }
