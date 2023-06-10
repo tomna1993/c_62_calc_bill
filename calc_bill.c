2b0b598679876d8e6a8e643eb2d6d2911ef2ca48
TomasNagy <tomi.nagy@outlook.com>
2023-06-10 17:26:45 +0200

    feat: open file

--- calc_bill.c
+++ calc_bill.c
@@ -3,6 +3,8 @@
 
 #define CHARS_MAX 30
 #define ITEMS_MAX 30
+#define EXIT_ERROR 1
+#define EXIT_SUCCESS 0
 
 struct Menu
 {
@@ -14,4 +16,16 @@ int main(void)
 {
     struct Menu snackBar[ITEMS_MAX];
       
+    
+    FILE *filePointer = fopen("snack_bar.txt", "r");
+
+    if (filePointer == NULL)
+    {
+        printf ("Cannot open the file!\n");
+        return EXIT_ERROR;
+    }
+
+    
+
+    return EXIT_SUCCESS;   
 }
